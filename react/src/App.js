c76f37fd2896d115da766ab68ee4fe9b343609e0
Maxim Mazurok <maxim@mazurok.com>
2024-01-02 12:53:04 +1100

    oops

--- react/src/App.js
+++ react/src/App.js
@@ -2,12 +2,21 @@ import { useEffect, useState } from "react";
 
 export default function MyApp() {
   const [count, setCount] = useState(0);
+  const [start] = useState(Date.now());
 
   useEffect(() => {
     setInterval(() => {
       setCount((count) => count + 1);
     }, 10);
-  });
+  }, []);
 
-  return <h1>React count is {count}</h1>;
+  return (
+    <div>
+      <h1>React count is {count}</h1>
+      <h2>
+        Count increments times per second:
+        {((count / (Date.now() - start)) * 1000).toFixed(2)}
+      </h2>
+    </div>
+  );
 }
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 
 const count = ref(0);
+const start = Date.now();
 
 setInterval(() => {
   count.value++;
@@ -9,5 +10,11 @@ setInterval(() => {
 </script>
 
 <template>
-  <h1>Vue count is {{ count }}</h1>
+  <div>
+    <h1>Vue count is {{ count }}</h1>
+    <h2>
+      Count increments times per second:
+      {{ ((count / (Date.now() - start)) * 1000).toFixed(2) }}
+    </h2>
+  </div>
 </template>
